b85955a6c83a44c6353ebd1c525e2c0b9c1ff8dd
ThinkWD <thinkwd@foxmail.com>
2024-09-18 15:57:29 +0800

    update

--- lx_module/network.py
+++ lx_module/network.py
@@ -3,6 +3,27 @@ import torch
 
 ###########################################################
 #
+#  特殊通用结构 自定义实现
+#
+###########################################################
+def softmax(X):
+    """softmax 函数
+    1. 对每个项求幂 (使用exp)
+    2. 对每一行求和得到每个样本的规范化常数
+    3. 将每一行除以其规范化常数, 确保结果的和为 1
+    """
+    X_exp = torch.exp(X)
+    partition = X_exp.sum(1, keepdim=True)
+    return X_exp / partition  # (广播机制)
+
+
+def relu(X):
+    """激活函数 relu, 就是 max(x, 0)"""
+    return torch.max(X, torch.zeros_like(X))
+
+
+###########################################################
+#
 #  网络结构 自定义实现
 #
 ###########################################################
@@ -30,19 +51,19 @@ class net_linear_regression_custom(object):
         return self.w, self.b
 
     def __call__(self, X):
-        return torch.matmul(X, self.w) + self.b
+        return torch.matmul(X, self.w) + self.b  # 不需要预处理, 没有输出层, 只有一层全连接层
 
 
 def net_softmax_regression(num_inputs, num_outputs):
-    """网络结构: softmax 回归模型
+    """网络结构: softmax 回归
     Args:
         num_inputs (int): 输入特征向量的长度, 决定权重参数数量
         num_outputs (int): 输出向量的长度, 即类别总数, 决定输出维度和偏移参数数量
     """
     net = torch.nn.Sequential(
-        torch.nn.Flatten(),  # 第一层: Flatten 展平层. 用于将原始图像(三维)展平为向量(一维)
-        torch.nn.Linear(num_inputs, num_outputs),  # 第二层: Linear 全连接层
-        # 第三层: softmax 层. 它没有显式的定义在网络结构中, 是因为 CrossEntropyLoss 中已经包含了 softmax 不要重复定义
+        torch.nn.Flatten(),  # 输入数据预处理: 将原始图像(三维)展平为向量(一维)
+        torch.nn.Linear(num_inputs, num_outputs),  # 第一层: Linear 全连接层
+        # 输出层: softmax 层. 它没有被显式定义是因为 CrossEntropyLoss 中已经包含了 softmax, 不要重复定义
     )
     # 参数初始化函数(lambda): 当 m 是 torch.nn.Linear 类型时初始化其权重, 否则什么也不做
     init_weights = lambda m: torch.nn.init.normal_(m.weight, std=0.01) if isinstance(m, torch.nn.Linear) else None
@@ -52,15 +73,7 @@ def net_softmax_regression(num_inputs, num_outputs):
 
 class net_softmax_regression_custom(object):
     def __init__(self, num_inputs, num_outputs):
-        """网络结构: Softmax 回归. 解决分类问题. 初始化网络参数函数
-
-        注意事项:
-            此处默认图片是灰度图片
-            我们的输入图片的形状是 (1, w, h), 它是一个三维的输入。
-            但是对于 Softmax 回归来讲, 输入需要是一个向量(一维).
-            所以我们需要将这个图片展平拉长, 拉成一个长度为 1*w*h 的向量.
-            当然拉成向量会失去图片本身的空间信息, 这个问题会在卷积神经网络章节继续讨论.
-
+        """网络结构: Softmax 回归
         Args:
             num_inputs (int): 输入特征向量的长度, 决定权重参数数量
             num_outputs (int): 输出向量的长度, 即类别总数, 决定输出维度和偏移参数数量
@@ -72,17 +85,7 @@ class net_softmax_regression_custom(object):
     def parameters(self):
         return [self.w, self.b]
 
-    def softmax(self, X):
-        """softmax 函数
-        1. 对每个项求幂 (使用exp)
-        2. 对每一行求和得到每个样本的规范化常数
-        3. 将每一行除以其规范化常数, 确保结果的和为 1
-        """
-        X_exp = torch.exp(X)
-        partition = X_exp.sum(1, keepdim=True)
-        return X_exp / partition  # (广播机制)
-
     def __call__(self, X):
-        temp = X.reshape((-1, self.w.shape[0]))  # 第一层: Flatten 展平层. 用于将原始图像(三维)展平为向量(一维)
-        temp = torch.matmul(temp, self.w) + self.b  # 第二层: Linear 全连接层
-        return self.softmax(temp)  # 第三层: softmax 层, 将预测值转为类别标签
+        X = X.reshape((-1, self.w.shape[0]))  # 输入数据预处理: 将原始图像(三维)展平为向量(一维)
+        X = torch.matmul(X, self.w) + self.b  # 第一层: Linear 全连接层
+        return softmax(X)  # 输出层: softmax 层, 将预测值转为类别标签
