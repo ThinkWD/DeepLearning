f6e762c2a11fdb7247f75ce11be6b5fef21517b8
ThinkWD <thinkwd@foxmail.com>
2024-09-27 14:51:49 +0800

    udpate

---
+++ 006_实战_预测房价.py
@@ -0,0 +1,21 @@
+from lx_module import dataset
+from lx_module import optimizer
+from lx_module import network
+from lx_module import loss_func
+from lx_module import uitls
+
+###########################################################################
+#
+#
+#   kaggle 房价预测比赛
+#
+#
+###########################################################################
+
+
+def main():
+    print("init")
+
+
+if __name__ == "__main__":
+    main()
