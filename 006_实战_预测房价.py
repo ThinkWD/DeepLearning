56ab6e66f1c4fbca854993043bd37c51d5f0569e
ThinkWD <thinkwd@foxmail.com>
2024-09-27 17:49:46 +0800

    update

--- 006_实战_预测房价.py
+++ 006_实战_预测房价.py
@@ -3,18 +3,56 @@ from lx_module import optimizer
 from lx_module import network
 from lx_module import loss_func
 from lx_module import uitls
+import torch
 
 ###########################################################################
 #
 #
 #   kaggle 房价预测比赛
+#   网址：https://www.kaggle.com/c/house-prices-advanced-regression-techniques
+#   数据集：./dataset/HousePricesAdvanced
 #
 #
 ###########################################################################
 
 
+# def log_rmse(net, features, labels):
+#     # 对 y 和 y_hat 都做个 log, 然后再计算损失
+#     # 为了在取对数时进一步稳定该值，将小于1的值设置为1
+#     clipped_preds = torch.clamp(net(features), 1, float('inf'))
+#     rmse = torch.sqrt(loss(torch.log(clipped_preds), torch.log(labels)))
+#     return rmse.item()
+
+
+def train_regression(net, opt, loss, data, num_epochs, log="log"):
+    animator = uitls.Animator(legend=['train loss'])  # yscale='log',
+    train_iter, test_iter = data.get_iter()
+    for ep in range(1, num_epochs + 1):
+        train_loss, _ = uitls.train_epoch(net, opt, loss, train_iter)
+        animator.add(ep, train_loss)
+        print(f"[{log}] epoch {ep}\t, train loss: {train_loss:.6f}")
+    train_loss, _ = uitls.evaluate(net, loss, train_iter)
+    animator.add(num_epochs + 1, train_loss)
+    print(f"[{log}] Training completed, train loss: {train_loss:.6f}")
+
+
 def main():
-    print("init")
+    ### >>> 初始化数据集和超参数 <<< ###########################################
+    learn_rate = 0.03  # (超参数)训练的学习率
+    num_epochs = 20  # (超参数)训练遍历数据集的次数
+    batch_size = 64  # (超参数)训练的批大小 (一次读取的数据数量)
+    num_workers = 8  # 加载数据集使用的工作线程数
+    weight_decay = 0.001  # 权重衰减参数 (实际一般使用 1e-4 ~ 1e-2)
+    data = dataset.Dataset_HousePricesAdvanced(batch_size, num_workers)
+
+    ### >>> 确定模型结构和超参数 <<< ###########################################
+    num_inputs = 79  # 输入特征向量长度, 由数据集决定
+    num_hiddens = []  # (超参数)隐藏层的数量和大小
+    dropout = []  # (超参数)隐藏层丢弃的概率
+    net = network.net_multilayer_perceptrons_custom(num_inputs, 1, num_hiddens, dropout)
+    opt = optimizer.opt_sgd_custom(net.parameters(), learn_rate, weight_decay)
+    loss = loss_func.loss_squared_custom()
+    train_regression(net, opt, loss, data, num_epochs, "custom")
 
 
 if __name__ == "__main__":
--- lx_module/dataset.py
+++ lx_module/dataset.py
@@ -1,5 +1,6 @@
 import time
 import torch
+import pandas
 import numpy as np
 import torchvision  # 对于计算机视觉实现的一个库
 import matplotlib.pyplot as plt
@@ -120,3 +121,58 @@ class Dataset_FashionMNIST(object):
             continue
         time_end = time.time()
         print(f'batch_size={batch_size}, num_workers={num_workers}, used_time={time_end - time_start:.2f} s')
+
+
+# kaggle 房价预测数据集: https://www.kaggle.com/c/house-prices-advanced-regression-techniques
+class Dataset_HousePricesAdvanced(object):
+    def __init__(self, batch_size=64, num_workers=8, save_path="./dataset"):
+        self.batch_size = batch_size
+        self.num_workers = num_workers
+
+        ### >>> 从文件中加载数据集 <<< ##########################################################################
+        train_data = pandas.read_csv(f'{save_path}/HousePricesAdvanced/train.csv')
+        test_data = pandas.read_csv(f'{save_path}/HousePricesAdvanced/test.csv')
+        # print(train_data.shape)
+        # print(train_data.iloc[0:4, [0, 1, 2, 3, -3, -2, -1]])
+
+        ### >>> 数据打包 <<< ##########################################################################
+        # 训练集去掉第一列的 ID 和最后一列的标签(成交房价) - 测试集不包括标签 所以只去掉第一列的 ID 即可
+        # 将训练集和测试集打包
+        # 如果训练时没有测试数据, 可以只在训练集上计算均值和方差, 然后应用到测试数据中
+        all_features = pandas.concat((train_data.iloc[:, 1:-1], test_data.iloc[:, 1:]))
+
+        ### >>> 数据预处理(数值类) <<< ##########################################################################
+        # 提取数值类数据的索引 (排除文本类数据)
+        numeric_features = all_features.dtypes[all_features.dtypes != 'object'].index
+        # 数据标准化 (将所有数值特征的均值变成0方差变成1) (每个数值都减去这一列的均值然后除以这一列的方差)
+        all_features[numeric_features] = all_features[numeric_features].apply(lambda x: (x - x.mean()) / (x.std()))
+        # 将缺失值置为均值 (在标准化数据之后，所有均值变成0了)
+        all_features[numeric_features] = all_features[numeric_features].fillna(0)
+
+        ### >>> 数据预处理(非数值类) <<< ##########################################################################
+        # # 编码之前: 查看哪些列是文本类数据, 以及这些列有多少个类别
+        # for object in all_features.dtypes[all_features.dtypes == 'object'].index:
+        #     print(object.ljust(20), len(all_features[object].unique())) # ljust 使打印数据更美观
+
+        # 对于文本类数据, 使用独热编码替换它们 (dummy_na=True 将缺失值也视为一个单独的类)
+        all_features = pandas.get_dummies(all_features, dummy_na=True)
+        all_features = all_features.astype(np.float32)  # 确保所有列都是数值类型
+
+        n_train = train_data.shape[0]
+        self.train_X = torch.tensor(all_features[:n_train].values, dtype=torch.float32)
+        self.train_y = torch.tensor(train_data.SalePrice.values.reshape(-1, 1), dtype=torch.float32)
+        self.test_X = torch.tensor(all_features[n_train:].values, dtype=torch.float32)
+        self.test_y = torch.zeros((self.test_X.shape[0], 1))  # 数据集中没有 test 部分的标签, 为了保持一致性全部设为 0
+
+    def get_iter(self, batch_size=0, num_workers=0):
+        if batch_size <= 0:
+            batch_size = self.batch_size
+        if num_workers <= 0:
+            num_workers = self.num_workers
+        train_arrays = (self.train_X, self.train_y)
+        train_dataset = torch.utils.data.TensorDataset(*train_arrays)
+        train_iter = torch.utils.data.DataLoader(train_dataset, batch_size, shuffle=True, num_workers=num_workers)
+        test_arrays = (self.test_X, self.test_y)
+        test_dataset = torch.utils.data.TensorDataset(*test_arrays)
+        test_iter = torch.utils.data.DataLoader(test_dataset, batch_size, shuffle=False, num_workers=num_workers)
+        return train_iter, test_iter
