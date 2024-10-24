0dc87f77734492dbad635a544e2158f40d8dc1d0
ThinkWD <thinkwd@foxmail.com>
2024-10-24 13:09:36 +0800

    update

--- 011_GoogLeNet.py
+++ 011_GoogLeNet.py
@@ -10,9 +10,25 @@ from lx_module import uitls
 class Inception(torch.nn.Module):
     # Inception 很可能得名于电影《盗梦空间》，因为电影中的一句话 "We need to go deeper"。
     # Inception 块解决了多大的卷积核最合适的问题，具体思路就是小学生才做选择，而我全都要。
+    #                         ┏━━━━━━━━━━━━━━━━━━━━━┓
+    #         ┌──────────────>┃  Concatenation(256) ┃<──────────────┐
+    #         │               ┗━━━━━━━━━━━━━━━━━━━━━┛               │
+    #         │                    ↑           ↑                    │
+    #         │         ┏━━━━━━━━━━━━━━━┓ ┏━━━━━━━━━━━━━━━┓ ┏━━━━━━━━━━━━━━━┓
+    #         │         ┃ 3x3 Conv(128) ┃ ┃ 5x5 Conv(32)  ┃ ┃ 1x1 Conv(32)  ┃
+    # ┏━━━━━━━━━━━━━━━┓ ┗━━━━━━━━━━━━━━━┛ ┗━━━━━━━━━━━━━━━┛ ┗━━━━━━━━━━━━━━━┛
+    # ┃ 1x1 Conv(64)  ┃         ↑                 ↑                 ↑
+    # ┗━━━━━━━━━━━━━━━┛ ┏━━━━━━━━━━━━━━━┓ ┏━━━━━━━━━━━━━━━┓ ┏━━━━━━━━━━━━━━━┓
+    #         ↑         ┃ 1x1 Conv(96)  ┃ ┃ 1x1 Conv(16)  ┃ ┃  3x3 MaxPool  ┃
+    #         │         ┗━━━━━━━━━━━━━━━┛ ┗━━━━━━━━━━━━━━━┛ ┗━━━━━━━━━━━━━━━┛
+    #         │                    ↑           ↑                    ↑
+    #         │                  ┏━━━━━━━━━━━━━━━┓                  │
+    #         └──────────────────┃   Input(192)  ┃──────────────────┘
+    #                            ┗━━━━━━━━━━━━━━━┛
     # 它将输入复制 4 份，送入四个线路，每个线路使用不同大小的卷积核从不同空间大小中提取信息。
     # 每个线路中都有一个 1x1 卷积层可以改变通道数，并且用合适的填充使输入输出的尺寸一致。
     # 每个线路输出的通道数是超参数，输出时会在通道维度上将四条线路的结果组合在一起。
+    #
     # 反直觉的是，这个复杂结构的参数量和计算复杂度反而比使用单纯的 3*3 或 5*5 卷积层要低的多！
     # Inception 块不仅增加了特征提取的多样性，而且参数变少，计算量还变低了。
     def __init__(self, in_channels, c1, c2, c3, c4, **kwargs):  # c1~c4是每条路径的输出通道数
@@ -43,33 +59,53 @@ def GoogLeNet(in_channels, num_outputs):
     # GoogLeNet 中 google 的 l 大写是为了致敬 LeNet
     # GoogLeNet 吸收了 NiN 中串联网络的思想，并在此基础上做了改进。
     # GoogLeNet 提出了 Inception 块，解决了什么样大小的卷积核最合适的问题。
+    # GoogLeNet 由于使用了 Inception 块，模型参数较少，计算复杂度较低。
+    # GoogLeNet 使用了 9 个 Inception 块，是第一个达到上百层的网络。
+    # 它的缺点是过于复杂，超参数太多，因此不是特别受欢迎
+    # https://blog.csdn.net/shuzfan/article/details/50738394
 
-    # 第一段：7*7 和 64 通道的卷积层，最后使用池化层将尺寸减半通道加倍
+    # stage1: 宽高减半两次，通道 in_channels -> 192
     stage1 = torch.nn.Sequential(
-        torch.nn.Conv2d(in_channels, 64, kernel_size=7, stride=2, padding=3),
+        torch.nn.Conv2d(in_channels, 64, kernel_size=7, stride=2, padding=3),  # 宽高减半
         torch.nn.ReLU(),
-        torch.nn.MaxPool2d(kernel_size=3, stride=2, padding=1),
+        torch.nn.MaxPool2d(kernel_size=3, stride=2, padding=1),  # 宽高减半
     )
-    # 第二段：1*1 卷积层，
+    # stage2: 宽高减半，通道 64 -> 192
     stage2 = torch.nn.Sequential(
         torch.nn.Conv2d(64, 64, kernel_size=1),
         torch.nn.ReLU(),
-        torch.nn.Conv2d(64, 192, kernel_size=3, padding=1),
+        torch.nn.Conv2d(64, 192, kernel_size=3, padding=1),  # 通道 64 -> 192
         torch.nn.ReLU(),
-        torch.nn.MaxPool2d(kernel_size=3, stride=2, padding=1),
+        torch.nn.MaxPool2d(kernel_size=3, stride=2, padding=1),  # 宽高减半
     )
-
-    net = torch.nn.Sequential(
-        nin_block(in_channels, 96, kernel_size=11, strides=4, padding=0),
-        torch.nn.MaxPool2d(3, stride=2),
-        nin_block(96, 256, kernel_size=5, strides=1, padding=2),
-        torch.nn.MaxPool2d(3, stride=2),
-        nin_block(256, 384, kernel_size=3, strides=1, padding=1),
-        torch.nn.MaxPool2d(3, stride=2),
-        nin_block(384, num_outputs, kernel_size=3, strides=1, padding=1),
-        torch.nn.AdaptiveAvgPool2d((1, 1)),
-        torch.nn.Flatten(),  # 将四维的输出转成二维的输出，其形状为(批量大小,10)
+    # stage3: 宽高减半，通道 192 -> 256 -> 480
+    stage3 = torch.nn.Sequential(
+        Inception(192, 64, (96, 128), (16, 32), 32),  # 输入 192 通道，输出 64+128+32+32=256 通道
+        Inception(256, 128, (128, 192), (32, 96), 64),  # 输入 256 通道，输出 128+192+96+64=480 通道
+        torch.nn.MaxPool2d(kernel_size=3, stride=2, padding=1),  # 宽高减半
     )
+    # stage4: 宽高减半，通道 480 -> 512 -> 512 -> 512 -> 528 -> 832
+    stage4 = torch.nn.Sequential(
+        Inception(480, 192, (96, 208), (16, 48), 64),  # 输入 480 通道，输出 512 通道
+        Inception(512, 160, (112, 224), (24, 64), 64),  # 输入 512 通道，输出 512 通道
+        Inception(512, 128, (128, 256), (24, 64), 64),  # 输入 512 通道，输出 512 通道
+        Inception(512, 112, (144, 288), (32, 64), 64),  # 输入 512 通道，输出 528 通道
+        Inception(528, 256, (160, 320), (32, 128), 128),  # 输入 528 通道，输出 832 通道
+        torch.nn.MaxPool2d(kernel_size=3, stride=2, padding=1),  # 宽高减半
+    )
+    # stage5: 宽高减半，通道 832 -> 832 -> 1024
+    stage5 = torch.nn.Sequential(
+        Inception(832, 256, (160, 320), (32, 128), 128),  # 输入 832 通道，输出 832 通道
+        Inception(832, 384, (192, 384), (48, 128), 128),  # 输入 832 通道，输出 1024 通道
+        torch.nn.AdaptiveAvgPool2d((1, 1)),  # 宽高减半 5 次，得到 7*7，最后被平均池化为 1*1
+        torch.nn.Flatten(),
+    )
+    # 分类头，加一个全连接层避免卷积层需要直接输出类别数的通道。
+    clas_head = torch.nn.Sequential(
+        torch.nn.Linear(1024, num_outputs),  # 1 * 1 * 1024
+    )
+    # 组合完整网络
+    net = torch.nn.Sequential(stage1, stage2, stage3, stage4, stage5, clas_head)
 
     # [xavier 初始化] 参数初始化函数: 当 m 是 torch.nn.Linear 权重时执行 xavier 初始化
     def init_weights(m):
@@ -79,7 +115,7 @@ def GoogLeNet(in_channels, num_outputs):
     net.apply(init_weights)
     net = net.to(device=uitls.try_gpu())
     # 打印网络结构, 每层的输出大小及参数量
-    torchsummary.summary(net, input_size=(in_channels, 224, 224))
+    torchsummary.summary(net, input_size=(in_channels, 96, 96))
     return net
 
 
@@ -89,7 +125,7 @@ def main():
     num_epochs = 10  # (超参数)训练遍历数据集的次数
     batch_size = 128  # (超参数)训练的批大小 (一次读取的数据数量)
     num_workers = 10  # 加载数据集使用的工作线程数
-    resize_pipeline = [torchvision.transforms.Resize(224), torchvision.transforms.ToTensor()]
+    resize_pipeline = [torchvision.transforms.Resize(96), torchvision.transforms.ToTensor()]
     data = dataset.Dataset_FashionMNIST(batch_size, num_workers, resize_pipeline)
 
     ### >>> 确定模型结构和超参数 <<< ###########################################
